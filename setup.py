0ef969b56aa831e9df8ce317a6a0b7ab54d287ab
Pr2 <Pr2@github.com>
2018-09-01 14:19:19 +0200

    OscamStatus Plugin by puhvogel 2011-2012
    Release 0.62-r1
    
    This is free software; you can redistribute it and/or modify it under
    the terms of the GNU General Public License as published by the Free
    Software Foundation; either version 2, or (at your option) any later
    version.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import setup_translate
+
+pkg = 'Extensions.OscamStatus'
+setup (name = 'enigma2-plugin-extensions-oscamstatus',
+       version = '0.62-r1',
+       description = 'shows status of your oscam server',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'},
+       package_data = {pkg: ['*.png', '*.xml', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass = setup_translate.cmdclass, # for translation
+      )
